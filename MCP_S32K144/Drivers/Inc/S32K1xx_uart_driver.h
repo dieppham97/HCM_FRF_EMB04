53e53110cf5352698e528efa2c57b03944b0441c
Diep Pham Van <142334704+dieppham97@users.noreply.github.com>
2024-08-27 11:44:15 +0700

    Update S32K1xx_uart_driver.h

--- MCP_S32K144/Drivers/Inc/S32K1xx_uart_driver.h
+++ MCP_S32K144/Drivers/Inc/S32K1xx_uart_driver.h
@@ -37,7 +37,7 @@ typedef struct
  *Possible options for USART_Mode				(TE bit and RE bit in CTRL)
  */	
 #define UART_MODE_ONLY_TX					0
-#define UART_MODE_ONLY_RX 				1
+#define UART_MODE_ONLY_RX 					1
 #define UART_MODE_TXRX 						2
 
 
@@ -55,64 +55,64 @@ typedef struct
 #define UART_BAUD_230400 					230400
 #define UART_BAUD_460800 					460800
 #define UART_BAUD_921600 					921600
-#define UART_BAUD_2M 							2000000
-#define UART_BAUD_3M 							3000000
+#define UART_BAUD_2M 						2000000
+#define UART_BAUD_3M 						3000000
 
 /*
  *@USART_NoOfStopBits
- *Possible options for USART_NoOfStopBits		(Stop bit in baudrate)
+ *Possible options for USART_NoOfStopBits			(Stop bit in baudrate)
  */
-#define UART_STOPBITS_1	  				0
-#define UART_STOPBITS_2	  				1
+#define UART_STOPBITS_1	  					0
+#define UART_STOPBITS_2	  					1
 
 /*
  *@USART_WordLength
- *Possible options for USART_WordLength			(M bit in CTRL)
+ *Possible options for USART_WordLength				(M bit in CTRL)
  */
-#define UART_WORDLEN_8BITS 				0
-#define UART_WORDLEN_9BITS  			1
+#define UART_WORDLEN_8BITS 					0
+#define UART_WORDLEN_9BITS  					1
 
 /*
  *@USART_ParityControl
- *Possible options for USART_ParityControl	(PF,PE in CTRL)
+ *Possible options for USART_ParityControl			(PF,PE in CTRL)
  */
-#define UART_PARITY_EN_ODD   			2
-#define UART_PARITY_EN_EVEN  			1
-#define UART_PARITY_DISABLE   		0
+#define UART_PARITY_EN_ODD   					2
+#define UART_PARITY_EN_EVEN  					1
+#define UART_PARITY_DISABLE   					0
 
 /*
  *@USART_OverSampling
  *Possible options for USART_OverSampling
  */
-#define UART_OSR4   							3
-#define UART_OSR5  								4
-#define UART_OSR6   							5
-#define UART_OSR7   							6
-#define UART_OSR8   							7
-#define UART_OSR9   							8
-#define UART_OSR10  							9
-#define UART_OSR11  							10
-#define UART_OSR12  							11
-#define UART_OSR13  							12
-#define UART_OSR14 								13
-#define UART_OSR15  							14
-#define UART_OSR16  							15
-#define UART_OSR17  							16
-#define UART_OSR18  							17
-#define UART_OSR19  							18
-#define UART_OSR20  							19
-#define UART_OSR21  							20
-#define UART_OSR22  							21
-#define UART_OSR23  							22
-#define UART_OSR24  							23
-#define UART_OSR25  							24
-#define UART_OSR26  							25
-#define UART_OSR27  							26
-#define UART_OSR28  							27
-#define UART_OSR29								28
-#define UART_OSR30  							29
-#define UART_OSR31  							30
-#define UART_OSR32  							31
+#define UART_OSR4   						3
+#define UART_OSR5  						4
+#define UART_OSR6   						5
+#define UART_OSR7   						6
+#define UART_OSR8   						7
+#define UART_OSR9   						8
+#define UART_OSR10  						9
+#define UART_OSR11  						10
+#define UART_OSR12  						11
+#define UART_OSR13  						12
+#define UART_OSR14 						13
+#define UART_OSR15  						14
+#define UART_OSR16  						15
+#define UART_OSR17  						16
+#define UART_OSR18  						17
+#define UART_OSR19  						18
+#define UART_OSR20  						19
+#define UART_OSR21  						20
+#define UART_OSR22  						21
+#define UART_OSR23  						22
+#define UART_OSR24  						23
+#define UART_OSR25  						24
+#define UART_OSR26  						25
+#define UART_OSR27  						26
+#define UART_OSR28  						27
+#define UART_OSR29						28
+#define UART_OSR30  						29
+#define UART_OSR31  						30
+#define UART_OSR32  						31
 
 /*
  *@USART_ITMODE
@@ -126,7 +126,7 @@ typedef struct
  */
 #define UART_BUSY_IN_RX 					1
 #define UART_BUSY_IN_TX 					2
-#define UART_READY 								0
+#define UART_READY 						0
 
 
 
